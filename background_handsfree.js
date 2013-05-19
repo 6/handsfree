a19783e1ac2ab69d85990a4a54e080a578b072ac
Peter Graham <peterghm@gmail.com>
2013-05-18 23:38:35 -0700

    Support different recording state on different tabs

--- background_handsfree.js
+++ background_handsfree.js
@@ -1,4 +1,4 @@
-var recording = false;
+var recording = {};
 
 function setStartIcon() {
   chrome.browserAction.setIcon({path: "start.png"});
@@ -9,15 +9,20 @@ function setStopIcon() {
 }
 
 chrome.browserAction.onClicked.addListener(function(tab) {
-  recording = !recording;
+  if(typeof recording[tab.id] == "undefined") {
+    recording[tab.id] = true;
+  }
+  else {
+    recording[tab.id] = !recording[tab.id];
+  }
   chrome.tabs.getSelected(null, function(tab) {
-    chrome.tabs.sendMessage(tab.id, {recording: recording});
-    return recording ? setStopIcon() : setStartIcon();
+    chrome.tabs.sendMessage(tab.id, {recording: recording[tab.id]});
+    return recording[tab.id] ? setStopIcon() : setStartIcon();
   });
 });
 
-chrome.tabs.onUpdated.addListener(function( tabId , info ) {
-  if(info.status == "complete" ) {
-    chrome.tabs.sendMessage(tabId, {recording: recording});
+chrome.tabs.onUpdated.addListener(function(tabId, info) {
+  if(info.status == "complete") {
+    chrome.tabs.sendMessage(tabId, {recording: recording[tabId]});
   }
 });
